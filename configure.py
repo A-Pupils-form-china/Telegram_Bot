e4e9987f8a3d3dc01822884d70902fee0a35d3fd
lion <njy1822574439@outlook.com>
2022-03-16 11:56:40 +0800

    第一个版本，存在一些bug，无法很好地部署为个人使用，以后会解决

---
+++ configure.py
@@ -0,0 +1,7 @@
+class configure:
+    def __init__(self):
+        self.time_gap = 0
+        self.TOKEN = '填你的机器人的token'
+        self.MY_CHAT_ID = 你的id
+        self.GROUP_ID = 群组id
+        self.CHANNEL_ID = 频道id
